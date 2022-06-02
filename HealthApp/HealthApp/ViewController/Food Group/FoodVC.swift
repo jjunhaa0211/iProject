1ac508e8772548b1e049e479429bbed60e57052a
junha <goodjunha@gmail.com>
2022-06-02 21:26:40 +0900

    ADD:: serchBer Radius

--- HealthApp/HealthApp/ViewController/Food Group/FoodVC.swift
+++ HealthApp/HealthApp/ViewController/Food Group/FoodVC.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class FoodVC: UIViewController {
 
+    //Make - IBOutlet
+    @IBOutlet var searchFilterSegment: UISegmentedControl!
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
+    @IBOutlet var searchIdicator: UIActivityIndicatorView!
+    
+    @IBOutlet var seachButton: UIButton!
+//---------------------------------
+    
+    //Make - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         print("FoodVC - viewDidLoad called")
+        
+        self.seachButton.layer.cornerRadius = 10
+        
+        self.searchBar.searchBarStyle = .minimal
+        
+        
+    }
+    
+    //Make - IBAction
+    @IBAction func searchFilterValueChanged(_ sender: UISegmentedControl) {
     }
 
 
