5d8e4966878526780f99813d135e04820fd58bef
junha <goodjunha@gmail.com>
2022-08-06 13:50:49 +0900

    ADD:: city, image,radomLabel change

--- SimpleWeather/SimpleWeather/weatherViewController.swift
+++ SimpleWeather/SimpleWeather/weatherViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class weatherViewController: UIViewController {
 
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var weatherImageView: UIImageView!
+    @IBOutlet var temperatureLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    let cities = ["Seoul","Tokyo","LA","Seattle"]
+    let weathers = ["cloud.fill",
+                    "sun.max.fill",
+                    "wind",
+                    "cloud.sun.rain.fill"]
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        
+        cityLabel.text = cities.randomElement()!
+        
+        let imageName = weathers.randomElement()
+        weatherImageView.image = UIImage(systemName: imageName!)?.withRenderingMode(.alwaysOriginal)
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLable.text = "\(randomTemp)"
+        print("도시, 온도, 날씨이미지 변경")
     }
 }
