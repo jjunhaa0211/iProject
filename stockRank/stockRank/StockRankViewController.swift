707b489243ac26b462951e621b590164d9145002
junha <goodjunha@gmail.com>
2022-08-07 12:06:06 +0900

    ADD:: mainVIew

---
+++ stockRank/stockRank/StockRankViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StockRankViewController.swift
+//  stockRank
+//
+//  Created by 박준하 on 2022/08/07.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ stockRank/stockRank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stockRank
+//
+//  Created by 박준하 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
