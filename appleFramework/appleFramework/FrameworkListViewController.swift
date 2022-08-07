b14731241e9e8b31b7904464b1ae7288ef9e6caf
junha <goodjunha@gmail.com>
2022-08-07 23:48:54 +0900

    ADD:: mainView

--- appleFramework/appleFramework/FrameworkListViewController.swift
+++ appleFramework/appleFramework/FrameworkListViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class FrameworkListViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+    
+    let list : [AppleFramework] = AppleFramework.list
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension FrameworkListViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return list.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else { return UICollectionViewCell() }
+        
+        let framework = list[indexPath.item]
+        cell.configure(framework)
+        return cell
     }
-    */
+    
+    
+}
 
+extension FrameworkListViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let interItemSpacing: CGFloat = 10
+        
+        let width = (collectionView.bounds.width - interItemSpacing * 2) / 3
+        
+        let height = width * 1.5
+        return CGSize(width: width, height: height)
+    }
+    
+ 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
 }
