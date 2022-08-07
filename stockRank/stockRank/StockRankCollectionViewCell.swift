b793ce092ff7da0297191f502804f348c1075623
junha <goodjunha@gmail.com>
2022-08-07 13:44:02 +0900

    ADD:: diff system color

--- stockRank/stockRank/StockRankCollectionViewCell.swift
+++ stockRank/stockRank/StockRankCollectionViewCell.swift
@@ -20,10 +20,16 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         companyIconImageView.image = UIImage(named: stock.imageName)
         companyNameLabel.text = stock.name
         companyPriceLabel.text = "\(convertToCurremcyFormat(price: stock.price)) 원"
+        var color: UIColor
+        
+        if stock.diff > 0 {
+            color = UIColor.systemRed
+        } else {
+            color = UIColor.systemBlue
+        }
+        diffLabel.textColor = color
         diffLabel.text = "\(stock.diff)%"
     }
-    
-    
     //소수점 3자리마다 ,를 찍어주는 코드
     func convertToCurremcyFormat(price: Int) -> String {
         let numberFormatter = NumberFormatter()
