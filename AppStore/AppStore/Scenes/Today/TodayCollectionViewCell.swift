ac4510f557e8153a8385be4486aac5d45fabcd91
junha <goodjunha@gmail.com>
2022-09-04 22:37:40 +0900

    🌹:: imageCell

--- AppStore/AppStore/Scenes/Today/TodayCollectionViewCell.swift
+++ AppStore/AppStore/Scenes/Today/TodayCollectionViewCell.swift
@@ -9,6 +9,41 @@ import UIKit
 import SnapKit
 
 final class TodayCollectionViewCell : UICollectionViewCell {
+    
+    private lazy var titleLabel : UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var subTitleLabel : UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var descriptionlable : UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12.0
+        imageView.backgroundColor = .gray
+        
+        return imageView
+    }()
+    
     func setup() {
         backgroundColor = .black
     }
