7123c9326b94367a519684a5392327a6ac9b482f
junha <goodjunha@gmail.com>
2022-09-04 22:49:06 +0900

    🌧:: titleCell

--- AppStore/AppStore/Scenes/Today/TodayCollectionViewCell.swift
+++ AppStore/AppStore/Scenes/Today/TodayCollectionViewCell.swift
@@ -37,6 +37,7 @@ final class TodayCollectionViewCell : UICollectionViewCell {
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
+        //imageview의 크기를 벗어나지 않기 위해서 사용
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 12.0
         imageView.backgroundColor = .gray
@@ -45,6 +46,39 @@ final class TodayCollectionViewCell : UICollectionViewCell {
     }()
     
     func setup() {
-        backgroundColor = .black
+
+        setupSubViews()
+        
+        subTitleLabel.text = "서브타이틀"
+        descriptionlable.text = "설명설명"
+        titleLabel.text = "앱의 이름"
+    }
+}
+
+private extension TodayCollectionViewCell {
+    func setupSubViews() {
+        [imageView,titleLabel,subTitleLabel,descriptionlable].forEach { addSubview($0) }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.top.equalToSuperview().inset(24.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(subTitleLabel)
+            $0.trailing.equalTo(subTitleLabel)
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(4.0)
+        }
+        
+        descriptionlable.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24.0)
+            $0.trailing.equalToSuperview().inset(24.0)
+            $0.bottom.equalToSuperview().inset(24.0)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
