3974954c154b90721e408cbb97a9f89ac38ed71f
junha <goodjunha@gmail.com>
2022-09-09 21:12:48 +0900

    😀:: tableView

--- SubwayStation/SubwayStation/StationSearchViewController.swift
+++ SubwayStation/SubwayStation/StationSearchViewController.swift
@@ -9,21 +9,66 @@ import SnapKit
 import UIKit
 
 class StationSearchViewController: UIViewController {
+    
+    private var numberOfCell: Int = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems() {
         //searchbar 위에 큰 타이틀
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착 정보"
         
         let searchController = UISearchController()
-        
         //searchbar 안에 문구
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요"
-        searchController.obscuresBackgroundDuringPresentation = false //임시
+        //true가 되면 주변이 흐릿해진다
+        searchController.obscuresBackgroundDuringPresentation = false
+        
+        searchController.searchBar.delegate = self
         
         navigationItem.searchController = searchController
     }
 
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+}
+
+extension StationSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 10
+        tableView.isHidden = false
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 0
+        tableView.isHidden = true
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        
+        return cell
+    }
 }
