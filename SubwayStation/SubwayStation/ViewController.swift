6ad2c5ceeb6025d65078ff93828d67572e7926f2
junha <goodjunha@gmail.com>
2022-09-09 20:22:52 +0900

    .DS_Store removed

---
+++ SubwayStation/SubwayStation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SubwayStation
+//
+//  Created by 박준하 on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
