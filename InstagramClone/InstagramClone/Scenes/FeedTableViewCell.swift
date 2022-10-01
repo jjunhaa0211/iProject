1abe238564b4a065dd843fafe824538eae62f1ea
junha <goodjunha@gmail.com>
2022-10-01 23:46:32 +0900

    🐊:: cell setup

--- InstagramClone/InstagramClone/Scenes/FeedTableViewCell.swift
+++ InstagramClone/InstagramClone/Scenes/FeedTableViewCell.swift
@@ -43,4 +43,84 @@ final class FeedTableViewCell: UITableViewCell {
         
         return button
     }()
+    
+    private lazy var currentLikedCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 13.0, weight: .semibold)
+        label.text = "박준하님 외 여러명이 좋아합니다."
+        
+        return label
+    }()
+    
+    private lazy var contensLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 13.0, weight: .medium)
+        label.numberOfLines = 5
+        label.text = "하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다 하하 코딩이 너무 재밌다"
+        
+        return label
+    }()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 11.0, weight: .semibold)
+        label.text = "1일 전"
+        
+        return label
+    }()
+    
+    func setup() {
+        [
+            postImageView,
+            likeButton,
+            commentButton,
+            directMessageButton,
+            bookmarkButton,
+            currentLikedCountLabel,
+            contensLabel,
+            dateLabel
+        ].forEach { addSubview($0) }
+        
+        postImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.equalToSuperview()
+            //1 : 1 비율이 된다
+            $0.height.equalTo(postImageView.snp.width)
+        }
+        
+        let buttonWidth: CGFloat = 24.0
+        let buttonInset: CGFloat = 16.0
+        
+        likeButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(buttonInset)
+            $0.top.equalTo(likeButton.snp.top)
+            $0.width.equalTo(buttonWidth)
+            $0.height.equalTo(buttonWidth)
+        }
+        
+        commentButton.snp.makeConstraints {
+            $0.leading.equalTo(likeButton.snp.trailing).offset(12.0)
+            $0.top.equalTo(likeButton.snp.top)
+            $0.width.equalTo(buttonWidth)
+            $0.height.equalTo(buttonWidth)
+        }
+        
+        directMessageButton.snp.makeConstraints {
+            $0.leading.equalTo(commentButton.snp.trailing).offset(12.0)
+            $0.top.equalTo(commentButton.snp.top)
+            $0.width.equalTo(buttonWidth)
+            $0.height.equalTo(buttonWidth)
+        }
+        
+        bookmarkButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(buttonInset)
+            $0.top.equalTo(likeButton.snp.top)
+            $0.width.equalTo(buttonInset)
+            $0.height.equalTo(buttonInset)
+        }
+    }
 }
