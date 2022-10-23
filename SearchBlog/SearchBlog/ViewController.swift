1718ae775486a4e132d85df82857cc8567f4047c
junha <goodjunha@gmail.com>
2022-10-23 23:35:45 +0900

    ⭐️:: view setting

---
+++ SearchBlog/SearchBlog/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SearchBlog
+//
+//  Created by 박준하 on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
