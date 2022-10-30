40b34d333d02b2a55288a825212ce461806ce33b
junha <goodjunha@gmail.com>
2022-10-30 16:35:10 +0900

    🪓:: del librarys

--- SearchBlog/SearchBlog/SceneDelegate.swift
+++ SearchBlog/SearchBlog/SceneDelegate.swift
@@ -14,10 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-                window = UIWindow(frame: UIScreen.main.bounds)
-                window?.windowScene = windowScene
-                window?.rootViewController = ViewController()
-                window?.makeKeyAndVisible()
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let rootViewController = MainViewController()
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window?.makeKeyAndVisible()
     }
 }
 
