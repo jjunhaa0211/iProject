a5ff239b0b016136daa27a43fd21f1fae08d4527
junha <goodjunha@gmail.com>
2022-11-21 10:44:42 +0900

    😆:: coustm SearchBar

--- SearchBlog/SearchBlog/MainViewController.swift
+++ SearchBlog/SearchBlog/MainViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
+    let disposeBag = DisposeBag()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        bind()
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+        
+    }
+    
+    private func attribute() {
+        title = "다음 블로그 검색"
+        view.backgroundColor = .white
+    }
+    
+    private func layout() {
+        
     }
 }
 
---
+++ SearchBlog/SearchBlog/SearchBar.swift
@@ -0,0 +1,81 @@
+//
+//  SearchBar.swift
+//  SearchBlog
+//
+//  Created by 박준하 on 2022/11/21.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RxRelay
+import SnapKit
+import Then
+
+class SearchBar: UISearchBar {
+    let disposeBag = DisposeBag()
+    
+    let searchButton = UIButton()
+    
+    let searchButtonTapped = PublishRelay<Void>()
+    
+    var shouldLoadResult = Observable<String>.of("")
+    
+    override init(frame: CGFloat) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+        Observable
+            .merge(
+                self.rx.searchButtonClicked.asObservable(),
+                searchButton.rx.tap.asObservable()
+            )
+            .bind(to: searchButtonTapped)
+            .disposed(by: disposeBag)
+        
+        searchButtonTapped
+            .asSignal()
+            .emit(to: self.rx.endEditing)
+            .disposed(by: disposeBag)
+        
+        self.shouldLoadResult = searchButtonTapped
+            .withLatestFrom(self.rx.text) { $1 ?? ""}
+            .filter { !$0.isEmpty }
+            .distinctUntilChanged()
+    }
+    
+    private func attribute() {
+        searchButton.setTitle("검색", for: .normal)
+        searchButton.setTitleColor(.systemBlue, for: .normal)
+    }
+    
+    private func layout() {
+        addSubview(searchButton)
+        
+        searchTextField.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(12)
+            $0.trailing.equalTo(searchButton.snp.leading).offset(-12)
+            $0.centerY.equalToSuperview()
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+    }
+}
+
+extension Reactive where Base: SearchBar {
+    var endEditing: Binder<Void> {
+        return Binder(base) { base, _ in
+            base.endEditing(true)
+        }
+    }
+}
+
