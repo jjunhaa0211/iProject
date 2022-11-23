662b778275727b94a7569e00fbeca1673cfb1abb
junha <goodjunha@gmail.com>
2022-11-23 20:02:00 +0900

    🌈:: alertForErrorMessage

--- SearchBlog/SearchBlog/Presentation/MainViewController/MainViewController.swift
+++ SearchBlog/SearchBlog/Presentation/MainViewController/MainViewController.swift
@@ -31,12 +31,104 @@ class MainViewController: UIViewController {
     }
     
     private func bind() {
+        
+        let blogResult = searchBar.shouldLoadResult
+            .flatMapLatest { query in
+                SearchBlogNetwork().searchBlog(query: query)
+            }
+            .share()
+        
+        let blogValue = blogResult
+            .compactMap { data -> DKBlog? in
+                guard case .success(let value) = data else {
+                    return nil
+                }
+                
+                return value
+            }
+        
+        let blogError = blogResult
+            .compactMap { data -> String? in
+                guard case .failure(let error) = data else {
+                    return nil
+                }
+                return error.localizedDescription
+            }
+        
+        
+        //네트워크로 가져온거 타입변환
+        let cellData = blogValue
+            .map { blog -> [BlogListCellData] in
+                return blog.documents
+                    .map { doc in
+                        let thumbnailURL = URL(string: doc.thumbnail ?? "")
+                        return BlogListCellData(
+                            thumbnailURL: thumbnailURL,
+                            name: doc.name,
+                            title: doc.title,
+                            datetime: doc.datetime)
+                    }
+            }
+        
+        //filterview를 선택했을 때 나오는 alertsheet를 선택했을 때의 type
+        let sortedType = alertActionTapped
+            .filter {
+                switch $0 {
+                case .title, .datetime:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .startWith(.title)
+        
+        //mainview에서 Listview롤 보내기
+        Observable
+            .combineLatest(
+                sortedType,
+                cellData
+            ) { type, data -> [BlogListCellData] in
+                switch type {
+                case .title:
+                    return data.sorted { $0.title ?? "" < $1.title ?? ""}
+                case .datetime:
+                    return data.sorted { $0.datetime ?? Date() > $1.datetime ?? Date() }
+                default:
+                    return data
+                }
+            }
+            .bind(to: listView.cellData)
+            .disposed(by: disposeBag)
+        
+        
         let alertshhetForSorting = listView.headerView.sortButtonTapped
             .map { _ -> Alert in
                 return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
             }
         
-        alertshhetForSorting
+        let alertSheetForSorting = listView.headerView.sortButtonTapped
+            .map { _ -> Alert in
+                return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
+            }
+        
+        let alertForErrorMessage = blogError
+            .do(onNext: { message in
+                print("error: \(message ?? "")")
+            })
+            .map { _ -> Alert in
+                return (
+                    title: "앗!",
+                    message: "예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+                    actions: [.confirm],
+                    style: .alert
+                )
+            }
+        
+        Observable
+            .merge(
+                alertSheetForSorting,
+                alertForErrorMessage
+            )
             .asSignal(onErrorSignalWith: .empty())
             .flatMapLatest { alert -> Signal<AlertAction> in
                 let alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: alert.style)
