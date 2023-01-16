85fb6e84f87c2f8d981f6508ac9400542044fb77
junha <goodjunha@gmail.com>
2023-01-16 19:04:38 +0900

    ☄️:: Entities

---
+++ ConvenienceStoreProject/ConvenienceStoreProject/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  ConvenienceStoreProject
+//
+//  Created by 박준하 on 2023/01/16.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ ConvenienceStoreProject/ConvenienceStoreProject/Entities/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  ConvenienceStoreProject
+//
+//  Created by 박준하 on 2023/01/16.
+//
+
+import Foundation
+
+struct LocationData: Decodable {
+    let documents: [KLDocument]
+}
