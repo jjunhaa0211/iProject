073dd7748927f31e6f71a4c895ed6d5b8a9b2bef
junha <goodjunha@gmail.com>
2023-09-06 09:26:56 +0900

    스윈젝트 프젝 세팅

---
+++ SwinjectTest/SwinjectTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwinjectTest
+//
+//  Created by 박준하 on 2023/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
